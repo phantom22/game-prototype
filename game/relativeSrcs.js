d8cfed82c78924f3247886d99203e439b3bea7cf
Maksym Naumenko <maksymnaumenko2002@mail.ru>
2019-08-11 23:59:59 +0300

    Add files via upload

--- game/relativeSrcs.js
+++ game/relativeSrcs.js
@@ -1,13 +1,12 @@
 // making the game instance accessible for debug purposes
 let instance;
 
-(function(){
-
+(function() {
   // getting the website url string
   let L = String(document.location);
   // getting the root folder of the game
-  const ROOT = L.slice(0,L.slice(0,L.lastIndexOf("/")).lastIndexOf("/")) + "/";
-  
+  const ROOT = L.slice(0, L.slice(0, L.lastIndexOf("/")).lastIndexOf("/")) + "/";
+
   // creating the "tool.js" script and appending it to the body
   let tScript = document.createElement("script");
   tScript.src = ROOT + "scripts/tool.js";
@@ -15,7 +14,6 @@ let instance;
 
   // onload of the "tool.js"
   tScript.onload = function() {
-
     // creating the "levels.js" script and appending it to the body
     let lScript = document.createElement("script");
     lScript.src = ROOT + "scripts/levels.js";
@@ -23,7 +21,6 @@ let instance;
 
     // onload of the "levels.js"
     lScript.onload = function() {
-
       // creating the "game.js" script and appending it to the body
       let gScript = document.createElement("script");
       gScript.src = ROOT + "scripts/game.js";
@@ -31,62 +28,108 @@ let instance;
 
       // onload of the "game.js"
       gScript.onload = function() {
-
         // getting the difficulty and the map number from the website url
-        let l = LEVELS; let a = L.slice(-12).split("d=")[1].split("&m=");
+        let l = LEVELS;
+        let a = L.slice(-12)
+          .split("d=")[1]
+          .split("&m=");
 
         // after all the .js scripts are loaded creating a game
-        instance = new gameInstance({position:true,moveDistance:1,vision:{initialRange:l[a[0]].range,tokensPerRangeLoss:l[a[0]].loss,maxRange:l[a[0]].range+2,minRange:2},gamemode:l[a[0]].gamemode},{grid:l[0][a[1]].grid,meta:`${l[0][a[1]].meta}`,bannedTilesFromRandomizing:l[0][a[1]].bannedTilesFromRandomizing,madness:{minRangeMovesBeforeDistortion:10,distortionLoop:15,playerDirectionDistorsion:0.05,distortionScaleMultiplier:2,loss:-0.025,gain:0.005,cap:0.25,coins:{true:{madnessMultiplier:0.4},false:{madnessMultiplier:0.6}}},coins:{true:{quantity:10,tokenRewards:{base:"*1.8",random:{added:[4,"*1.3"],removed:[0,"*0.7"]}}},false:{quantity:15,tokenRewards:{base:"*1.2",random:{added:[3,"*0.8"],removed:[0,"*0.3"]}}}}},true);
+        instance = new gameInstance(
+          {
+            spawnPoint: true,
+            moveDistance: 1,
+            vision: {
+              initialRange: l[a[0]].range,
+              tokensPerRangeLoss: l[a[0]].loss,
+              maxRange: l[a[0]].range + 2,
+              minRange: 2
+            },
+            gamemode: l[a[0]].gamemode
+          },
+          {
+            grid: l[0][a[1]].grid,
+            meta: `${l[0][a[1]].meta}`,
+            bannedTilesFromRandomizing: l[0][a[1]].bannedTilesFromRandomizing,
+            madness: {
+              minRangeMovesBeforeDistortion: 10,
+              distortionLoop: 15,
+              playerDirectionDistorsion: 0.05,
+              distortionScaleMultiplier: 2,
+              loss: -0.025,
+              gain: 0.005,
+              cap: 0.25,
+              coins: {
+                true: { madnessMultiplier: 0.4 },
+                false: { madnessMultiplier: 0.6 }
+              }
+            },
+            coins: {
+              true: {
+                quantity: 10,
+                tokenRewards: {
+                  base: "*1.8",
+                  random: { added: [4, "*1.3"], removed: [0, "*0.7"] }
+                }
+              },
+              false: {
+                quantity: 15,
+                tokenRewards: {
+                  base: "*1.2",
+                  random: { added: [3, "*0.8"], removed: [0, "*0.3"] }
+                }
+              }
+            }
+          },
+          {
+            state: true,
+            maxRegistryLength: 50000
+          }
+        );
 
         // adding an event listener for the keybord press event
-        document.addEventListener("keydown", function(event){
-
+        document.addEventListener("keydown", function(event) {
           // getting player position
-          let xy = instance.data.player.position; let k = event.keyCode; let x = xy[0]; let y = xy[1]; let s; let e; let id;
+          let xy = instance.data.entities.player1.position;
+          let k = event.keyCode;
+          let x = xy[0];
+          let y = xy[1];
+          let s;
+          let e;
+          let id;
 
           // if pressed "W"
-          if (k == 87) {s = [x,y-1]}
+          if (k == 87) {
+            s = [x, y - 1]
+          }
           // if pressed "S"
-          else if (k == 83) {s = [x,y+1]}
+          else if (k == 83) {
+            s = [x, y + 1]
+          }
           // if pressed "A"
-          else if (k == 65) {s = [x-1,y]}
+          else if (k == 65) {
+            s = [x - 1, y]
+          }
           // if pressed "D"
-          else if (k == 68) {s = [x+1,y]}
-          // if pressed "arrow up"
-          else if (k == 38) {instance.updateSelectedSlot(instance.data.player.hud.inventory.selectedSlot-1)}
-          // if pressed "arrow down"
-          else if (k == 40) {instance.updateSelectedSlot(instance.data.player.hud.inventory.selectedSlot+1)}
+          else if (k == 68) {
+            s = [x + 1, y]
+          }
 
           // if the new coordinates are defined (if one of the WASD buttons was pressed)
           if (s) {
-
             // register querySelector
             let r = instance.registry[`${s[0]}-${s[1]}`];
             // if the tile is found in the registry
             id = r ? r.id : undefined;
-            e = r ? r.element : undefined;
-
+            e = r ? r.element : undefined
           }
 
           // if the tile is found in the registry and its id in not 1 (wall) then move to that tile
-          if (e && e instanceof HTMLElement && id !== 1) {instance.playerMove(e)}
-
-        });
-
-        // adding event listener to the inventory slots tds
-        document.querySelectorAll(".hud td").forEach(v=>v.addEventListener("click",function(evt){
-
-          // getting the id of each clicked slot
-          let id = Number(evt.target.dataset.id);
-          // updating the selected slot
-          instance.updateSelectedSlot(id);
-          
-        }));
-
+          if (e && e instanceof HTMLElement && id !== 1) {
+            instance.entityMove("player1", e)
+          }
+        })
       }
-
     }
-
   }
-
-})();
+})()
